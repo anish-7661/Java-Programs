f428ed6a1fe4eee340127c4b37efc5a259614f9d
anish-7661 <115088255+anish-7661@users.noreply.github.com>
2022-10-07 18:20:25 +0530

    Edited the Program

--- Addition.java
+++ Addition.java
@@ -1,12 +1,22 @@
-class  Addition
-{
-	public static void main(String[] args) 
-	{
-		int sum = 0;
-		for(int i=0;i<args.length;i++)
-		{
-			sum+=Integer.parseInt(args[i]);
-		}
-		System.out.println("Addistion is "+ sum);
-	}
-}
+// Edited the Program
+import java.util.Scanner;  
+public class SumOfNumbers2  
+{  
+public static void main(String args[])  
+{  
+int x, y, sum;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the first number: ");  
+x = sc.nextInt();  
+System.out.print("Enter the second number: ");  
+y = sc.nextInt();  
+sum = sum(x, y);  
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+//method that calculates the sum  
+public static int sum(int a, int b)  
+{  
+int sum = a + b;  
+return sum;  
+}  
+} 
